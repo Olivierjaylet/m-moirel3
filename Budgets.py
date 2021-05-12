d04d399f1aacff65504da7116621192aa8038fe5
Olivierjaylet <48811167+Olivierjaylet@users.noreply.github.com>
2021-05-12 18:00:03 +0200

    Add files via upload

---
+++ Budgets.py
@@ -0,0 +1,228 @@
+budgetFr1011 = dict()
+budgetFr1011['Paris SG'] = 80
+budgetFr1011['Lyon'] = 150
+budgetFr1011['Monaco'] = 53
+budgetFr1011['Marseille'] = 140
+budgetFr1011["Lille"] = 55
+budgetFr1011["St Etienne"] = 55
+budgetFr1011["Bordeaux"] = 80
+budgetFr1011["Rennes"] = 45
+budgetFr1011["Nice"] = 30
+budgetFr1011["Montpellier"] = 33
+budgetFr1011["Toulouse"] = 40
+budgetFr1011["Caen"] = 31
+budgetFr1011["Valenciennes"] = 30
+budgetFr1011["Auxerre"] = 40
+budgetFr1011["Lorient"] = 35
+budgetFr1011["Nancy"] = 40
+budgetFr1011["Sochaux"]=40
+budgetFr1011["Lens"]=40
+budgetFr1011["Brest"]=23.5
+budgetFr1011["Arles"]=18
+
+budgetFr1415 = dict()
+budgetFr1415['Paris SG'] = 490
+budgetFr1415['Lyon'] = 116
+budgetFr1415['Monaco'] = 160
+budgetFr1415['Marseille'] = 105
+budgetFr1415["Lille"] = 65
+budgetFr1415["St Etienne"] = 50
+budgetFr1415["Bordeaux"] = 55
+budgetFr1415["Rennes"] = 40
+budgetFr1415["Nice"] = 40
+budgetFr1415["Montpellier"] = 40
+budgetFr1415["Toulouse"] = 32
+budgetFr1415["Caen"] = 26
+budgetFr1415["Lorient"] = 36
+budgetFr1415["Guingamp"] = 25
+budgetFr1415["Lens"]=46
+budgetFr1415["Bastia"]=22
+budgetFr1415["Reims"] = 30
+budgetFr1415["Evian Thonon Gaillard"] = 28
+budgetFr1415["Metz"] = 28
+budgetFr1415["Nantes"] = 32
+
+
+budgetFr1617 = dict()
+budgetFr1617['Paris SG'] = 500
+budgetFr1617['Lyon'] = 235
+budgetFr1617['Monaco'] = 145
+budgetFr1617['Marseille'] = 100
+budgetFr1617["Lille"] = 75
+budgetFr1617["St Etienne"] = 70
+budgetFr1617["Bordeaux"] = 60
+budgetFr1617["Rennes"] = 50
+budgetFr1617["Nantes"] = 40
+budgetFr1617["Nice"] = 42
+budgetFr1617["Montpellier"] = 42
+budgetFr1617["Toulouse"] = 35
+budgetFr1617["Metz"] = 30
+budgetFr1617["Caen"] = 29
+budgetFr1617["Dijon"] = 26
+budgetFr1617["Bastia"] = 28
+budgetFr1617["Angers"] = 25
+budgetFr1617["Guingamp"] = 26
+budgetFr1617["Lorient"] = 36
+budgetFr1617["Nancy"] = 30
+
+budgetFr1718 = dict()
+budgetFr1718['Paris SG'] = 540
+budgetFr1718['Lyon'] = 240
+budgetFr1718['Monaco'] = 170
+budgetFr1718['Marseille'] = 120
+budgetFr1718["Lille"] = 90
+budgetFr1718["St Etienne"] = 68
+budgetFr1718["Bordeaux"] = 65
+budgetFr1718["Rennes"] = 50
+budgetFr1718["Nantes"] = 45
+budgetFr1718["Nice"] = 45
+budgetFr1718["Montpellier"] = 43.5
+budgetFr1718["Toulouse"] = 34
+budgetFr1718["Metz"] = 33
+budgetFr1718["Caen"] = 32
+budgetFr1718["Dijon"] = 32
+budgetFr1718["Strasbourg"] = 30
+budgetFr1718["Angers"] = 28
+budgetFr1718["Guingamp"] = 26
+budgetFr1718["Troyes"] = 26
+budgetFr1718["Amiens"] = 25
+
+budgetFr1819 = dict()
+budgetFr1819['Paris SG'] = 500
+budgetFr1819['Lyon'] = 285
+budgetFr1819['Monaco'] = 215
+budgetFr1819['Marseille'] = 150
+budgetFr1819["Lille"] = 90
+budgetFr1819["St Etienne"] = 74
+budgetFr1819["Bordeaux"] = 70
+budgetFr1819["Rennes"] = 68
+budgetFr1819["Nantes"] = 60
+budgetFr1819["Nice"] = 50
+budgetFr1819["Montpellier"] = 41.4
+budgetFr1819["Toulouse"] = 34
+budgetFr1819["Nimes"] = 20
+budgetFr1819["Caen"] = 34
+budgetFr1819["Dijon"] = 35
+budgetFr1819["Strasbourg"] = 35
+budgetFr1819["Angers"] = 30
+budgetFr1819["Guingamp"] = 30
+budgetFr1819["Amiens"] = 36
+budgetFr1819["Reims"] = 40
+
+'''
+budgetAl1718 = dict()
+budgetAl1718["Bayern Munich"] = 
+budgetAl1718["Leverkusen"] = 
+budgetAl1718["Hamburg"] = 
+budgetAl1718["Augsburg"] = 
+budgetAl1718["Hertha"] = 
+budgetAl1718["Stuttgart"] = 
+budgetAl1718["Hoffenheim"] = 
+budgetAl1718["Werder Bremen"] = 
+budgetAl1718["Mainz"] = 
+budgetAl1718["Hannover"] = 
+budgetAl1718["Schalke 04"] = 
+budgetAl1718["RB Leipzig"] = 
+budgetAl1718["Wolfsburg"] = 
+budgetAl1718["Dortmund"] =
+budgetAl1718["Freiburg"] = 
+budgetAl1718["Ein Frankfurt"] = 
+budgetAl1718["M'gladbach"] = 
+budgetAl1718["FC Koln"] = 
+budgetAl1718["Hamburg"] = 
+budgetAl1718[""] = 
+'''
+
+
+budgetAl1617 = dict()
+budgetAl1617["Bayern Munich"] = 570
+budgetAl1617["Leverkusen"] = 275
+budgetAl1617["Hamburg"] = 84
+budgetAl1617["Augsburg"] = 62
+budgetAl1617["Hertha"] = 79.5
+budgetAl1617["Hoffenheim"] = 79
+budgetAl1617["Werder Bremen"] = 67.5 
+budgetAl1617["Mainz"] = 80
+budgetAl1617["Schalke 04"] = 202
+budgetAl1617["RB Leipzig"] = 79
+budgetAl1617["Wolfsburg"] = 178
+budgetAl1617["Dortmund"] = 360
+budgetAl1617["Freiburg"] = 44
+budgetAl1617["Ein Frankfurt"] = 41.5
+budgetAl1617["M'gladbach"] = 155
+budgetAl1617["FC Koln"] = 86
+budgetAl1617["Darmstadt"] = 27.5
+budgetAl1617["Ingolstadt"] = 36
+
+'''
+budgetAl1011 = dict()
+budgetAl1011["Bayern Munich"] = 570
+budgetAl1011["Leverkusen"] = 275
+budgetAl1011["Hamburg"] = 84
+budgetAl1011["Augsburg"] = 62
+budgetAl1011["Hertha"] = 79.5
+budgetAl1011["Hoffenheim"] = 79
+budgetAl1011["Werder Bremen"] = 67.5 
+budgetAl1011["Mainz"] = 80
+budgetAl1011["Schalke 04"] = 202
+budgetAl1011["RB Leipzig"] = 79
+budgetAl1011["Wolfsburg"] = 178
+budgetAl1011["Dortmund"] = 360
+budgetAl1011["Freiburg"] = 44
+budgetAl1011["Ein Frankfurt"] = 41.5
+budgetAl1011["M'gladbach"] = 155
+budgetAl1011["FC Koln"] = 86
+budgetAl1011["Darmstadt"] = 27.5
+budgetAl1011["Ingolstadt"] = 36'''
+
+
+budgetEs1819 = dict()
+budgetEs1819["Betis"] = 97
+budgetEs1819["Levante"] = 52
+budgetEs1819["Girona"] = 37
+budgetEs1819["Valladolid"] = 24
+budgetEs1819["Barcelona"] = 633
+budgetEs1819["Alaves"] = 39
+budgetEs1819["Celta"] = 51
+budgetEs1819["Espanol"] = 57
+budgetEs1819["Villarreal"] = 109
+budgetEs1819["Sociedad"] = 81
+budgetEs1819["Eibar"] = 41
+budgetEs1819["Huesca"] = 29
+budgetEs1819["Real Madrid"] = 567
+budgetEs1819["Getafe"] = 39
+budgetEs1819["Vallecano"] = 33
+budgetEs1819["Sevilla"] = 163
+budgetEs1819["Ath Bilbao"] = 88
+budgetEs1819["Leganes"] = 53
+budgetEs1819["Valencia"] = 165
+budgetEs1819["Ath Madrid"] = 293
+
+
+
+budgetAn1718 = dict()
+budgetAn1718["Arsenal"] = 417
+budgetAn1718["Leicester"] = 176
+budgetAn1718["Brighton"] = 157
+budgetAn1718["Man City"] = 548
+budgetAn1718["Chelsea"] = 472
+budgetAn1718["Burnley"] = 88
+budgetAn1718["Crystal Palace"] = 132
+budgetAn1718["Huddersfield"] = 135
+budgetAn1718["Everton"] = 204
+budgetAn1718["Stoke"] = 122
+budgetAn1718["Southampton"] = 157
+budgetAn1718["Swansea"] = 181
+budgetAn1718["Watford"] = 113
+budgetAn1718["Liverpool"] =361
+budgetAn1718["West Brom"] = 146
+budgetAn1718["Bournemouth"] = 93
+budgetAn1718["Man United"] = 655
+budgetAn1718["West Ham"] = 212
+budgetAn1718["Tottenham"] = 288
+budgetAn1718["Newcastle"] = 118
+
+
+
+
+
